559f9f5ab4d0e1f535ecb22ca6d17a3b11d21d7a
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-10-24 01:07:35 +0000

    TrafficLight

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
 
+import React from "react";
+import ReactDOM from 'react-dom/client';
 // include your styles into the webpack bundle
 import "../styles/index.css";
-
+import "bootstrap";
 //import your own components
-import Home from "./component/home.jsx";
-
+import { Home } from "./component/home";
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
+
+let numbers, a, b;
+
+numbers = [10, 20];
+a = numbers[0]
+b = numbers[1]
+console.log(a, b);
+
+ReactDOM.createRoot(document.getElementById('app')).render(<Home />);
